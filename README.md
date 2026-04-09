c39fc2141c1a13d5692229d650239a62a67561ea
A c h i t a N i l j u m r u s <achitaniljumrus@achitas-MacBook-Air.local>
2026-04-09 23:39:01 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ExScent Perfume Website
+This project is part of ITCS223: Introduction to Web Development, Faculty of Information and Communication Technology, Mahidol University.
---
+++ application.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require("dotenv");
+const mysql = require('mysql2');
+
+const app = express();
+const router = express.Router();
+
+require('dotenv').config();
+
+app.use(express.json());
+app.use(express.static('html'));
+app.use(express.urlencoded({ extended: true }));
+app.use(router)
+
+var connection = mysql.createConnection({
+    host        : process.env.MYSQL_HOST,
+    user        : process.env.MYSQL_USERNAME,
+    password    : process.env.MYSQL_PASSWORD,
+    database    : process.env.MYSQL_DATABASE
+});
+
+connection.connect(function(err){
+    if(err) throw err;
+    console.log(`Connected DB: ${process.env.MYSQL_DATABASE}`);
+});
+
+router.get('/', (req, res) => {
+    res.statusCode = 200;
+    console.log("Request at " + req.url);
+    res.sendFile(path.join(`${__dirname}/html/main.html`))
+})
+
+router.use((req, res, next) => {
+    console.log("Request at " + req.url);
+    console.log("404: Invalid accessed");
+    res.sendFile(path.join(`${__dirname}/html/error.html`))
+    res.status(404);
+})
+
+app.listen(process.env.PORT, function () {
+    console.log(`Server listening on port: ` + process.env.PORT);
+})
